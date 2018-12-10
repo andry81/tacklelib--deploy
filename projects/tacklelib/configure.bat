66ef953ed093c9659b911e2688904525e7d22b20
andry81 <andry@inbox.ru>
2018-12-10 18:49:11 +0000

    * new: tacklelib/scripts
    git-svn-id: svn+ssh://svn.code.sf.net/p/tacklelib/deploy/trunk@7 33b81f81-64fa-4653-8df1-23b06a6e4698

--- projects/tacklelib/configure.bat
+++ projects/tacklelib/configure.bat
@@ -17,20 +17,24 @@ set /A NEST_LVL+=1
   echo.set "TACKLELIB_DEPLOY.SVN.REPOROOT=https://%%SVN.HUB_ROOT%%/tacklelib/deploy"
   echo.set "TACKLELIB.SVN.REPOROOT=https://%%SVN.HUB_ROOT%%/tacklelib/tacklelib"
   echo.set "TACKLELIB_CMAKE.SVN.REPOROOT=https://%%SVN.HUB_ROOT%%/tacklelib/cmake"
+  echo.set "TACKLELIB_SCRIPTS.SVN.REPOROOT=https://%%SVN.HUB_ROOT%%/tacklelib/scripts"
   echo.set "TACKLELIB_EXAMPLES.SVN.REPOROOT=https://%%SVN.HUB_ROOT%%/tacklelib/examples"
   echo.
   echo.set "TACKLELIB_DEPLOY.GIT.ORIGIN=https://%%GIT.USER%%@%%GIT.HUB_ROOT%%/%%GIT.REPO_OWNER%%/tacklelib--deploy.git"
   echo.set "TACKLELIB.GIT.ORIGIN=https://%%GIT.USER%%@%%GIT.HUB_ROOT%%/%%GIT.REPO_OWNER%%/tacklelib.git"
   echo.set "TACKLELIB_CMAKE.GIT.ORIGIN=https://%%GIT.USER%%@%%GIT.HUB_ROOT%%/%%GIT.REPO_OWNER%%/tacklelib--cmake.git"
+  echo.set "TACKLELIB_SCRIPTS.GIT.ORIGIN=https://%%GIT.USER%%@%%GIT.HUB_ROOT%%/%%GIT.REPO_OWNER%%/tacklelib--scripts.git"
   echo.set "TACKLELIB_EXAMPLES.GIT.ORIGIN=https://%%GIT.USER%%@%%GIT.HUB_ROOT%%/%%GIT.REPO_OWNER%%/tacklelib--examples.git"
   echo.
   echo.set "TACKLELIB_DEPLOY.GIT2.ORIGIN=https://%%GIT2.USER%%@%%GIT2.HUB_ROOT%%/%%GIT2.REPO_OWNER%%/tacklelib-deploy.git"
   echo.set "TACKLELIB.GIT2.ORIGIN=https://%%GIT2.USER%%@%%GIT2.HUB_ROOT%%/%%GIT2.REPO_OWNER%%/tacklelib.git"
   echo.set "TACKLELIB_CMAKE.GIT2.ORIGIN=https://%%GIT2.USER%%@%%GIT2.HUB_ROOT%%/%%GIT2.REPO_OWNER%%/tacklelib-cmake.git"
+  echo.set "TACKLELIB_SCRIPTS.GIT2.ORIGIN=https://%%GIT2.USER%%@%%GIT2.HUB_ROOT%%/%%GIT2.REPO_OWNER%%/tacklelib-scripts.git"
   echo.set "TACKLELIB_EXAMPLES.GIT2.ORIGIN=https://%%GIT2.USER%%@%%GIT2.HUB_ROOT%%/%%GIT2.REPO_OWNER%%/tacklelib-examples.git"
   echo.
   echo.set "TACKLELIB.GIT3.ORIGIN=https://%%GIT3.USER%%@%%GIT3.HUB_ROOT%%/%%GIT3.REPO_OWNER%%/tacklelib.git"
   echo.set "TACKLELIB_CMAKE.GIT3.ORIGIN=https://%%GIT3.USER%%@%%GIT3.HUB_ROOT%%/%%GIT3.REPO_OWNER%%/tacklelib--cmake.git"
+  echo.set "TACKLELIB_SCRIPTS.GIT3.ORIGIN=https://%%GIT3.USER%%@%%GIT3.HUB_ROOT%%/%%GIT3.REPO_OWNER%%/tacklelib--scripts.git"
   echo.set "TACKLELIB_EXAMPLES.GIT3.ORIGIN=https://%%GIT3.USER%%@%%GIT3.HUB_ROOT%%/%%GIT3.REPO_OWNER%%/tacklelib--examples.git"
   echo.
 ) > "%~dp0configure.user.bat"
---
+++ projects/tacklelib/scripts/configure.bat
@@ -0,0 +1,25 @@
+@echo off
+
+echo.^>%~dpnx0
+
+setlocal
+
+if not defined NEST_LVL set NEST_LVL=0
+
+set /A NEST_LVL+=1
+
+(
+  echo.@echo off
+  echo.
+  echo.set "SVN.WCROOT_DIR=sf~tacklelib--scripts"
+  echo.set "GIT.WCROOT_DIR=gh~tacklelib--scripts"
+  echo.set "GIT2.WCROOT_DIR=bb~tacklelib--scripts"
+  echo.set "GIT3.WCROOT_DIR=gl~tacklelib--scripts"
+  echo.
+) > "%~dp0configure.user.bat"
+
+set /A NEST_LVL-=1
+
+if %NEST_LVL% LEQ 0 pause
+
+exit /b
