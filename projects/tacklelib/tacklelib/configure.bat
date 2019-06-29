7663731f6815b6e15af0d598dbb900f0b4010c43
andry81 <andry@inbox.ru>
2019-06-29 02:44:32 +0000

    2019.06.29: * changed: all user scripts auto self logging * refactor: configure.bat script
    git-svn-id: svn+ssh://svn.code.sf.net/p/tacklelib/deploy/trunk@18 33b81f81-64fa-4653-8df1-23b06a6e4698

--- projects/tacklelib/tacklelib/configure.bat
+++ projects/tacklelib/tacklelib/configure.bat
@@ -4,28 +4,7 @@ setlocal
 
 call "%%~dp0__init__.bat" || exit /b
 
-if %NEST_LVL%0 EQU 0 goto WITH_LOGGING
+set "CONFIGURE_DIR=%~dp0"
+set "CONFIGURE_DIR=%CONFIGURE_DIR:~0,-1%"
 
-rem no local logging if nested call
-call "%%BASE_SCRIPTS_ROOT%%\configure_directory.bat" "%~dp0" || exit /b
-
-exit /b 0
-
-:WITH_LOGGING
-rem to prevent pause call under logging
-set /A NEST_LVL+=1
-
-rem logging for all output if not nested call
-call "%%CONTOOLS_ROOT%%\get_datetime.bat"
-set "LOG_FILE_NAME_SUFFIX=%RETURN_VALUE:~0,4%_%RETURN_VALUE:~4,2%_%RETURN_VALUE:~6,2%_%RETURN_VALUE:~8,2%_%RETURN_VALUE:~10,2%_%RETURN_VALUE:~12,2%_%RETURN_VALUE:~15,3%"
-
-(
-  call "%%BASE_SCRIPTS_ROOT%%\configure_directory.bat" "%~dp0" || exit /b
-) 2>&1 | "%CONTOOLS_ROOT%\wtee.exe" "%CONFIGURE_DIR%\.log\%~n0_%LOG_NAME%.%LOG_FILE_NAME_SUFFIX%.log"
-
-rem to prevent pause call under logging
-set /A NEST_LVL-=1
-
-if %NEST_LVL% LEQ 0 pause
-
-exit /b 0
+call "%%BASE_SCRIPTS_ROOT%%\configure.bat" "%%CONFIGURE_DIR%%" || exit /b
