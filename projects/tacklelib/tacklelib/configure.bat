49b46788e4d3bc39a0e68a077731c65b1f3d52c9
andry81 <andry@inbox.ru>
2019-07-01 16:43:10 +0000

    2019.07.01: * new: partially switched to the `python` 3.x with the modules: `xonsh` (unix shell like environment, `.xsh` scripts) + `cmdix` (unix core utils implemented in the python) * new: `configure.xsh` script, calls from `configure.bat` interface scripts * new: `README_EN.known_issues.txt` file for known issues around the scripts * changed: partially disabled `configure.bat` script implementation (`configure.py.bat` script have has used instead) * changed: inner xonsh piping functionality is not used because is not stable yet (see the `README_EN.known_issues.txt` for the details) * changed: variables related to the python implementation extracted into `.yaml` configuration files (`repos.lst.in` files are left as is to the python csv parser) * changed: readme update
    2019.06.29:
    * changed: all user scripts auto self logging
    * changed: `configure_directory.bat` and `configure_project.bat` scripts merged into single `configure.bat` script
    * refactor: `configure.bat` script
    
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/tacklelib/deploy/trunk@20 33b81f81-64fa-4653-8df1-23b06a6e4698

--- projects/tacklelib/tacklelib/configure.bat
+++ projects/tacklelib/tacklelib/configure.bat
@@ -7,4 +7,4 @@ call "%%~dp0__init__.bat" || exit /b
 set "CONFIGURE_DIR=%~dp0"
 set "CONFIGURE_DIR=%CONFIGURE_DIR:~0,-1%"
 
-call "%%BASE_SCRIPTS_ROOT%%\configure.bat" "%%CONFIGURE_DIR%%" || exit /b
+call "%%BASE_SCRIPTS_ROOT%%\configure.py.bat" "%%CONFIGURE_DIR%%" || exit /b
