1a6b19180a2d79460ce568f136843014cd15b7a1
andry81 <andry@inbox.ru>
2020-01-05 15:20:37 +0000

    2020.01.04: * new: `pyxvcs` project as an example of self svn2git mirroring * new: `git_path_prefix` parameter in `git_repos.lst.in` configuration file * changed: back merge from pyxvcs project * refactor: moderate refactor
    git-svn-id: svn+ssh://svn.code.sf.net/p/tacklelib/deploy/trunk@36 33b81f81-64fa-4653-8df1-23b06a6e4698

--- projects/tacklelib/__init__.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-call "%%~dp0..\__init__.bat" || exit /b
-
-set "CONFIGURE_DIR=%~dp0"
-set "CONFIGURE_DIR=%CONFIGURE_DIR:~0,-1%"
-
-call "%%CONTOOLS_ROOT%%\load_config.bat" "%%CONFIGURE_DIR%%" "config.vars" || exit /b
